ed0cbf03c8a52de804c925e49bc95c0e95a682f6
Tarunk0201 <tarun.singh.7801@gmail.com>
2025-04-26 23:38:24 +0530

    dependency installed icon pack

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,12 @@ export default function Navbar() {
   return (
     <>
       <section>
-        <div className="flex justify-between pt-1 h-13 bg-amber-200">
+        <div className="flex justify-between pt-1 pl-25 pr-25 h-13 bg-amber-200">
           <h3 className="basis-64 pl-10 pt-2 h-13 font-bold text-3xl antialiased tracking-wide">
             Navbar
           </h3>
           <input
-            className="border-2 rounded-2xl w-xl h-11 p-2 basis-180 bg-amber-50"
+            className="border-2 rounded-2xl h-11 p-2 basis-180 mr-3 bg-amber-50"
             type="text"
             placeholder=" search"
           />
